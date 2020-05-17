536f00b3d164ee736fbbca9b58479340d6c2ee68
Mesko <willianmesko@hotmail.com>
2020-05-17 17:48:34 -0300

    first commit

---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,82 @@
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  user_id: string;
+  date: Date;
+}
+
+@injectable()
+class CreateAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({
+    provider_id,
+    user_id,
+    date,
+  }: IRequest): Promise<Appointment> {
+    const appointmentDate = startOfHour(date);
+
+    if (user_id === provider_id) {
+      throw new AppError('You cannot create  an appointment with yourself');
+    }
+
+    const hour = getHours(appointmentDate);
+    if (hour < 8 || hour > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm.',
+      );
+    }
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError('you cannot create an appointment in the past date');
+    }
+
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = await this.appointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: appointmentDate,
+    });
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para ${dateFormatted}`,
+    });
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d',
+      )}`,
+    );
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import '@shared/infra/typeorm';
+import '@shared/container';
+import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
+import AppError from '@shared/errors/AppError';
+import { errors } from 'celebrate';
+import routes from './routes';
+
+const app = express();
+
+app.use(rateLimiter);
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(routes);
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('🚀 Server started on port 3333!');
+});
